a41f5a11b626fc9f26093175610d8b34fe2a04b4
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-04 15:10:54 +0100

    day4

---
+++ day4/part1.js
@@ -0,0 +1,32 @@
+import { match } from 'assert'
+import fs from 'fs'
+
+const inputTxt = fs.readFileSync('./input.txt', 'utf-8')
+
+const FIELD_DELIMITER = ','
+
+const lines = inputTxt.split(/\r?\n/).map((line) =>
+  line.split(FIELD_DELIMITER).map((value) =>
+    value
+      .trim()
+      .split('-')
+      .map((x) => Number(x)),
+  ),
+)
+
+const result = lines.reduce(
+  (result, [[elf1Min, elf1Max], [elf2Min, elf2Max]]) => {
+    console.log('\n', elf1Min, elf1Max, '-', elf2Min, elf2Max)
+    if (
+      (elf1Min <= elf2Min && elf1Max >= elf2Max) ||
+      (elf2Min <= elf1Min && elf2Max >= elf1Max)
+    ) {
+      console.log('overlap')
+      result++
+    }
+    return result
+  },
+  0,
+)
+
+console.log('result', result)
---
+++ day4/part2.js
@@ -0,0 +1,32 @@
+import { match } from 'assert'
+import fs from 'fs'
+
+const inputTxt = fs.readFileSync('./input.txt', 'utf-8')
+
+const FIELD_DELIMITER = ','
+
+const lines = inputTxt.split(/\r?\n/).map((line) =>
+  line
+    .split(FIELD_DELIMITER)
+    .map((value) =>
+      value
+        .trim()
+        .split('-')
+        .map((x) => Number(x)),
+    )
+    .sort(([elf1Min, elf1Max], [elf2Min, elf2Max]) => elf1Min - elf2Min),
+)
+
+const result = lines.reduce(
+  (result, [[elf1Min, elf1Max], [elf2Min, elf2Max]]) => {
+    console.log('\n', elf1Min, elf1Max, '-', elf2Min, elf2Max)
+    if (elf2Min <= elf1Max) {
+      console.log('overlap')
+      result++
+    }
+    return result
+  },
+  0,
+)
+
+console.log('result', result)
