56982b98b404c3fc6ec91f7c57e11f4172905e4b
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-05 10:05:43 +0100

    day5

---
+++ day5/part2.js
@@ -0,0 +1,63 @@
+import { match } from 'assert'
+import fs from 'fs'
+
+const inputTxt = fs.readFileSync('./input.txt', 'utf-8')
+
+// const FIELD_DELIMITER = ','
+
+const lines = inputTxt.split(/\r?\n/)
+// .map((line) => line.split(FIELD_DELIMITER))
+
+const [initial, moves] = lines.reduce(
+  (result, line) => {
+    const [initial, moves] = result
+    if (line === '') {
+      moves.push(line)
+    } else if (moves.length) {
+      if (moves[0] === '') {
+        moves[0] = line
+      } else {
+        moves.push(line)
+      }
+    } else {
+      initial.push(line)
+    }
+    return result
+  },
+  [[], []],
+)
+
+const initialReversed = initial.slice(0, initial.length - 1).reverse()
+
+const numberOfStacks = (initialReversed[0].length + 1) / 4
+const emptyStacks = new Array(numberOfStacks).fill(undefined).map(() => [])
+const stacks = initialReversed.reduce((result, line) => {
+  let i = 0
+  while (i + 1 <= numberOfStacks) {
+    const value = line
+      .substring(i * 4, i * 4 + 4)
+      .trim()
+      .replace('[', '')
+      .replace(']', '')
+
+    if (value) {
+      result[i].push(value)
+    }
+
+    i++
+  }
+  return result
+}, emptyStacks)
+
+const parsedMoves = moves.map((move) => {
+  const parts = move.split(' ')
+  return [Number(parts[1]), Number(parts[3]) - 1, Number(parts[5]) - 1]
+})
+
+parsedMoves.forEach(([move, from, to]) => {
+  const take = stacks[from].splice(stacks[from].length - move)
+  stacks[to].push(...take)
+})
+
+const result = stacks.map((stack) => stack[stack.length - 1]).join('')
+console.log('result', result)
