86fb44a8028461f2c0fcf95d6640df1fd4c02f6c
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-06 08:17:15 +0100

    day6

---
+++ day6/part1.js
@@ -0,0 +1,22 @@
+import { match } from 'assert'
+import fs from 'fs'
+
+const inputTxt = fs.readFileSync('./input.txt', 'utf-8')
+
+// const FIELD_DELIMITER = ','
+
+const lines = inputTxt.split(/\r?\n/)
+// .map((line) => line.split(FIELD_DELIMITER))
+
+const input = lines[0]
+
+const areCharsDifferent = (str) => {
+  return [...new Set(str.split(''))].length === str.length
+}
+
+let i = 0
+while (!areCharsDifferent(input.substring(i, i + 4))) {
+  i++
+}
+
+console.log(i + 4)
