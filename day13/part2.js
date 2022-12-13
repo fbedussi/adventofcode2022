adae31e6ed6935814462cea656220ffdd04e76b2
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-13 23:55:31 +0100

    day13 - part 2

--- day13/part2.js
+++ day13/part2.js
@@ -2,98 +2,77 @@ import fs from 'fs'
 
 const inputTxt = fs.readFileSync('./input.txt', 'utf-8')
 
-const map = inputTxt.split(/\r?\n/).map((line) => line.split(''))
+const packets = inputTxt
+  .split(/\r?\n/)
+  .reduce((result, line) => {
+    if (line) {
+      result.push(eval(line))
+    }
+    return result
+  }, [])
+  .concat([[[2]], [[6]]])
 
-const startingPoints = []
-let E
-for (let y = 0; y < map.length; y++) {
-  for (let x = 0; x < map[y].length; x++) {
-    if (map[y][x] === 'S') {
-      map[y][x] = 'a'
+// console.log('packets', packets)
+
+const compare = (left, right) => {
+  if (!Array.isArray(left) && !Array.isArray(right)) {
+    if (left < right) {
+      return true
     }
-    if (map[y][x] === 'E') {
-      E = [x, y]
-      map[y][x] = 'z'
+    if (left > right) {
+      return false
     }
-    map[y][x] = map[y][x].charCodeAt(0) - 97
-    if (map[y][x] === 0) {
-      startingPoints.push([x, y])
-    }
+    return undefined
   }
+
+  if (!Array.isArray(left) || !Array.isArray(right)) {
+    return compare([].concat(left), [].concat(right))
+  }
+
+  let sorted = undefined
+  while (sorted === undefined && left.length && right.length) {
+    sorted = compare(left.shift(), right.shift())
+  }
+
+  if (sorted === undefined && !left.length && right.length) {
+    sorted = true
+  }
+
+  if (sorted === undefined && !right.length && left.length) {
+    sorted = false
+  }
+
+  return sorted
 }
 
-console.log('E', E)
-
-class Node {
-  constructor(parent, x, y) {
-    this.parent = parent
-    this.depth = parent ? parent.depth + 1 : 0
-    this.children = undefined
-    this.x = x
-    this.y = y
-  }
-}
-
-let visited = {}
-let queue = []
-let distancies = []
-const visitNode = (node) => {
-  visited[`${node.x},${node.y}`] = true
-
-  if (node.x === E[0] && node.y === E[1]) {
-    console.log('END', node.depth)
-    distancies.push(node.depth)
-    return
-  }
-
-  if (!node.children) {
-    node.children = []
-
-    const childrenCoords = []
-    if (node.x > 0) {
-      childrenCoords.push([node.x - 1, node.y])
-    }
-    if (node.y > 0) {
-      childrenCoords.push([node.x, node.y - 1])
-    }
-    if (node.x < map[node.y].length - 1) {
-      childrenCoords.push([node.x + 1, node.y])
-    }
-    if (node.y < map.length - 1) {
-      childrenCoords.push([node.x, node.y + 1])
-    }
-
-    childrenCoords
-      .filter(([x, y]) => {
-        const ancestor = x === node.parent?.x && y === node.parent?.y
-
-        const childValue = map[y][x]
-        const nodeValue = map[node.y][node.x]
-        const valueMatch = childValue <= nodeValue + 1
-
-        return !ancestor && valueMatch && !visited[`${x},${y}`]
-      })
-      .forEach((childCoord) => {
-        if (
-          !queue.find(
-            (node) => node.x === childCoord[0] && node.y === childCoord[1],
-          )
-        ) {
-          queue.push(new Node(node, ...childCoord))
-        }
-      })
-  }
-
-  const nextNode = queue.shift()
-  if (nextNode) {
-    visitNode(nextNode)
-  }
-}
-
-startingPoints.map((S) => {
-  visited = {}
-  queue = []
-  return visitNode(new Node(null, ...S))
+packets.sort((left, right) => {
+  const result = compare(
+    JSON.parse(JSON.stringify(left)),
+    JSON.parse(JSON.stringify(right)),
+  )
+  return result ? -1 : 1
 })
-const result = Math.min(...distancies)
+
+// console.log('sorted packets', packets)
+
+let divider1Index
+let divider2Index
+
+const divider1 = JSON.stringify([[2]])
+const divider2 = JSON.stringify([[6]])
+for (let i = 0; i < packets.length; i++) {
+  const item = JSON.stringify(packets[i])
+  if (item === divider1) {
+    divider1Index = i + 1
+  }
+  if (item === divider2) {
+    divider2Index = i + 1
+  }
+  if (divider1Index !== undefined && divider2Index !== undefined) {
+    break
+  }
+}
+
+const result = divider1Index * divider2Index
+
 console.log('result', result)
